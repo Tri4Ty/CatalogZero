b74dcbf7526735b47bcb554247c998ed3ada81f7
Tri4Ty <steven_thomas_12@hotmail.com>
2019-07-07 21:09:07 -0400

    overwriting default theme with custom theme component

--- dev/src/App.js
+++ dev/src/App.js
@@ -1,7 +1,33 @@
 import React from "react";
 import Button from '../../src/Button';
+import ThemeProvider from '../../src/ThemeProvider';
+
+const myCustomTheme = {
+    button: {
+        backgroundColor: "hotpink",
+        borderColor: "yellow",
+        color: "aqua"
+    }
+};
+
+function withCustomTheme(CustomComponent, theme) {
+    return function ThemedComponent(props) {
+        return (
+            <ThemeProvider theme={theme}>
+                <CustomComponent {...props} />
+            </ThemeProvider>
+        );
+    }
+}
+  
+const MyNewThemedButton = withCustomTheme(Button, myCustomTheme);
 
 // Simple application that only displays a button
-const App = () => <Button>Button</Button>;
+const App = () => (
+    <div>
+        <Button>Themed Button</Button>
+        <MyNewThemedButton>Custom Themed Button</MyNewThemedButton>
+    </div>
+)
 
 export default App;
--- dev/src/index.js
+++ dev/src/index.js
@@ -5,7 +5,7 @@ import theme from 'theme';
 
 import ThemeProvider from '../../src/ThemeProvider';
 import App from './App'
-
+ 
 ReactDOM.render(
     <ThemeProvider theme={theme}>
         <App />
